c5d11366dce6ffa46301ba5eb3bd8d244dc8b327
Sarowar Ahmed <113256659+sarowarahmed@users.noreply.github.com>
2023-12-26 22:58:05 +0000

    added variables in JS Chapter_01

---
+++ Chapter_01/variables.js
@@ -0,0 +1,12 @@
+const accountID = 345261
+let accountEmail = "sarowar@github.com"
+var accountCIty = "Jaipur"
+accountPassword = "764512"
+
+// accountID = 896745
+// console.log("accountID") const decleared variable are non changeble
+// we should not ues 'var' because of issue in block scope and functional scope
+
+let accountState;
+// console.table([accountID, accountEmail, accountPassword, accountCIty])
+console.table([accountID, accountEmail, accountPassword, accountCIty, accountState])
