62c1f0b86ff9c8f47903667db5a7810b1b5a1549
Sarowar Ahmed <113256659+sarowarahmed@users.noreply.github.com>
2023-12-27 00:01:08 +0000

    added datatypes in JS Chapter_01

---
+++ Chapter_01/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hello")
+
+
+let names = "sarowar"
+let age = 24
+let isLoggedIn = false
+let state;
+
+console.log(typeof names);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
